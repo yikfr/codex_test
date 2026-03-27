a36ba94f71b653f27664087019ed6be3e6366b27
yikfr <1204419111@qq.com>
2026-03-27 21:31:09 +0800

    Update chat.py

--- 挑战课题/pages/chat.py
+++ 挑战课题/pages/chat.py
@@ -37,7 +37,6 @@ if user_input:
         full_response = ""
 
         response = ask_deepseek(user_input, st.session_state.messages)
-        st.markdown(response, unsafe_allow_html=True)
 
         for char in response:
             full_response += char
