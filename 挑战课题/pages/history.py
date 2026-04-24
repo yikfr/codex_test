669ebe2abd0ae998c399b3ab40f89599bb689013
yikfr <1204419111@qq.com>
2026-04-24 16:35:36 +0800

    Update history.py

--- 挑战课题/pages/history.py
+++ 挑战课题/pages/history.py
@@ -22,7 +22,7 @@ def get_connection():
         database=st.secrets["mysql"]["database"],
         charset="utf8mb4",
         autocommit=True,
-        ssl={"ssl": {}}
+        ssl={"ssl": {}},
         init_command="SET time_zone='+08:00'" 
     )
 
